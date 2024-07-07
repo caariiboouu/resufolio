851217cfd3f2d2527686cfc5e42aad4bf34a6baf
Joel Cuthriell <joel@cuthriell.com>
2024-07-07 17:05:33 -0500

    writing and title and favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -19,7 +19,8 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>resufolio</title>
+        <title>Joel Cuthriell • UX</title>
+        <link rel="icon" type="image/x-icon" href="../favicon.ico" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body class="bg-[#1bafc6]">
